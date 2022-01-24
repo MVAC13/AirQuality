860822950c3aef859dec628976ed5947a8f38b24
Veronica <mv.arambula13@gmail.com>
2022-01-24 12:48:54 +0100

    changed code instead used time.sleep

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -3,27 +3,11 @@
     Required packages:
     - time
     - os
-    - apscheduler
 """
 
-import time
 import os
-from apscheduler.schedulers.background import BackgroundScheduler
+import time
 
-# calling the main script
-script = os.system("main.py")
-
-
-if __name__ == '__main__':
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(script, 'interval', hours=4)
-    scheduler.start()
-    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
-
-    try:
-        # this is here to simulate application activity (which keeps the main thread alive).
-        while True:
-            time.sleep(2)
-    except (KeyboardInterrupt, SystemExit):
-        # not strictly necessary if daemonic mode is enabled but should be done if possible
-        scheduler.shutdown()
+while True:
+  os.system("python C:\\Users\\Veronica\\PycharmProjects\\AirQuality\\scripts\\main.py")
+  time.sleep(2*60)
