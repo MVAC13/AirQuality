f0db0549d79063e9aeb396112b382ae56a0b50c1
Veronica <mv.arambula13@gmail.com>
2022-01-24 12:06:26 +0100

    moved them to the scripts folder

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -1,32 +1,29 @@
 """ scheduler.py
     Executes main.py every 4 hours
     Required packages:
-    - datetime
     - time
     - os
+    - apscheduler
 """
 
-from datetime import datetime
 import time
 import os
-
 from apscheduler.schedulers.background import BackgroundScheduler
 
-
-def train_model():
-    print('dask train_model! The time is: %s' % datetime.now())
+# calling the main script
+script = os.system("main.py")
 
 
 if __name__ == '__main__':
     scheduler = BackgroundScheduler()
-    scheduler.add_job(train_model, 'interval', hours=3)
+    scheduler.add_job(script, 'interval', hours=4)
     scheduler.start()
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
 
     try:
-        # This is here to simulate application activity (which keeps the main thread alive).
+        # this is here to simulate application activity (which keeps the main thread alive).
         while True:
             time.sleep(2)
     except (KeyboardInterrupt, SystemExit):
-        # Not strictly necessary if daemonic mode is enabled but should be done if possible
+        # not strictly necessary if daemonic mode is enabled but should be done if possible
         scheduler.shutdown()
